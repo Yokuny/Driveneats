0a28e18822c5c053be4d99995320dcd31f7ffffe
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-01-07 16:45:40 -0300

    chaging cards with input to just a normal div

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,14 @@
-function snackCards(elemento) {
-  const elementoEstilizado = document.querySelector(".indicaçãoDeSeleção");
+function snackCard(elemento) {
+  const elementoEstilizado = document.querySelector(".indicadorSnackCards");
   if (elementoEstilizado !== null) {
-    elementoEstilizado.classList.remove("indicaçãoDeSeleção");
+    elementoEstilizado.classList.remove("indicadorSnackCards");
   }
-  elemento.classList.add("indicaçãoDeSeleção");
+  elemento.classList.add("indicadorSnackCards");
+}
+function drinkCard(elemento) {
+  const elementoEstilizado = document.querySelector(".indicadorDrinkCards");
+  if (elementoEstilizado !== null) {
+    elementoEstilizado.classList.remove("indicadorDrinkCards");
+  }
+  elemento.classList.add("indicadorDrinkCards");
 }
