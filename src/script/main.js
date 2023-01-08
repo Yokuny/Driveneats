71ec83600914e731ff13895abbf99a97b7a320a2
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-01-07 21:29:49 -0300

    dessets done

--- src/script/main.js
+++ src/script/main.js
@@ -1,23 +1,20 @@
+function indicadorCheck(elementClass) {
+  const elementoEstilizado = document.querySelector(`.${elementClass}`);
+  if (elementoEstilizado !== null) {
+    elementoEstilizado.classList.remove(elementClass);
+  }
+}
 function snackCard(elemento) {
-  const elementoEstilizado = document.querySelector(".indicadorSnackCards");
+  indicadorCheck("indicadorSnackCards");
   const checkIcon = document.getElementsByClassName("snacks");
   console.log(checkIcon);
-  if (elementoEstilizado !== null) {
-    elementoEstilizado.classList.remove("indicadorSnackCards");
-  }
   elemento.classList.add("indicadorSnackCards");
 }
 function drinkCard(elemento) {
-  const elementoEstilizado = document.querySelector(".indicadorDrinkCards");
-  if (elementoEstilizado !== null) {
-    elementoEstilizado.classList.remove("indicadorDrinkCards");
-  }
+  indicadorCheck("indicadorDrinkCards");
   elemento.classList.add("indicadorDrinkCards");
 }
 function dessertCard(elemento) {
-  const elementoEstilizado = document.querySelector(".indicadorDessertCards");
-  if (elementoEstilizado !== null) {
-    elementoEstilizado.classList.remove("indicadorDessertCards");
-  }
+  indicadorCheck("indicadorDessertCards");
   elemento.classList.add("indicadorDessertCards");
 }
