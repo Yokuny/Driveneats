58f59fb0de1865cc06ab3bb9c4aca7d83a441ea8
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-01-07 23:04:47 -0300

    two functions to display and remove check icon

--- src/script/main.js
+++ src/script/main.js
@@ -4,24 +4,35 @@ function indicadorCheck(elementClass) {
     elementoEstilizado.classList.remove(elementClass);
   }
 }
-function removeCheckButton() {
-  const checkIcon = document.getElementsByClassName("snacks");
+function removeCheckButton(theClass) {
+  const checkIcon = document.querySelector(theClass).childNodes;
+  for (let i = 0; i < checkIcon.length; i++) {
+    if (checkIcon[i].childNodes[1]) {
+      console.log(
+        checkIcon[i].childNodes[1].childNodes[7].childNodes[3].classList.remove(
+          "cardCheckMarkVisible"
+        )
+      );
+    }
+  }
 }
 function placeCheckButton(element) {
   element.childNodes[1].childNodes[7].childNodes[3].classList.add("cardCheckMarkVisible");
 }
 function snackCard(elemento) {
+  removeCheckButton(".snacks");
   indicadorCheck("indicadorSnackCards");
-  removeCheckButton();
   placeCheckButton(elemento);
   elemento.classList.add("indicadorSnackCards");
 }
 function drinkCard(elemento) {
-  indicadorCheck("indicadorDrinkCards");
+  removeCheckButton(".drinks");
   placeCheckButton(elemento);
+  indicadorCheck("indicadorDrinkCards");
   elemento.classList.add("indicadorDrinkCards");
 }
 function dessertCard(elemento) {
+  removeCheckButton(".desserts");
   indicadorCheck("indicadorDessertCards");
   placeCheckButton(elemento);
   elemento.classList.add("indicadorDessertCards");
