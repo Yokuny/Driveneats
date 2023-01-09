f17be1b9e5995289b5818ca32f559c870554fa4d
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-01-09 11:38:56 -0300

    all done
    now i just have to see if the hub agree with me

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,7 @@
 let snack, drink, dessert;
-const submitButton = document.getElementById("submitButton");
+const reviewButton = document.getElementById("reviewOrder");
+const submitButton = document.getElementById("submitOrder");
 const cancelButton = document.getElementById("cancelOrder");
-
 const core = (() => {
   function removeCheckButton(theClass) {
     const checkIcon = document.querySelector(theClass).childNodes;
@@ -23,16 +23,13 @@ const core = (() => {
     element.childNodes[1].childNodes[7].childNodes[3].classList.add("cardCheckMarkVisible");
   }
   function checkAllData() {
-    if (snack && drink && dessert) {
-      return true;
-    }
-    return false;
+    return snack && drink && dessert;
   }
   function buttonActivation() {
-    const submitButton = document.getElementById("submitButton");
-    submitButton.textContent = "Fechar pedido";
-    submitButton.disabled = false;
-    submitButton.classList.add("activedButton");
+    const reviewBtn = document.getElementById("reviewOrder");
+    reviewBtn.textContent = "Fechar pedido";
+    reviewBtn.disabled = false;
+    reviewBtn.classList.add("activedButton");
   }
   const getElementData = (element) => {
     const escolha = element.childNodes[1].childNodes[3].textContent;
@@ -49,53 +46,54 @@ const core = (() => {
     }
     element.classList.add(elementClass);
   }
-  function aaa() {
-    console.log("aol");
+  function getNameAndAdress() {
+    const userName = prompt("digite seu nome:");
+    const userAdress = prompt("digite seu endereço:");
+    return { userName, userAdress };
   }
-
-  const submiteOrder = () => {
+  function cardConstructor(total) {
+    const aboutSnacks = document.getElementById("aboutSnacks");
+    aboutSnacks.childNodes[1].textContent = snack.escolha;
+    aboutSnacks.childNodes[3].textContent = `R$ ${snack.valor}`;
+    const aboutDrink = document.getElementById("aboutDrink");
+    aboutDrink.childNodes[1].textContent = drink.escolha;
+    aboutDrink.childNodes[3].textContent = `R$ ${drink.valor}`;
+    const aboutDessert = document.getElementById("aboutDessert");
+    aboutDessert.childNodes[1].textContent = dessert.escolha;
+    aboutDessert.childNodes[3].textContent = `R$ ${dessert.valor}`;
+    const totalPrice = document.getElementById("totalPrice");
+    totalPrice.childNodes[3].textContent = `R$ ${total}`;
+    const reviewOrder = document.getElementById("reviewCard");
+    reviewOrder.style.display = "flex";
+  }
+  function getOrderPrice() {
+    return (snack.valor + drink.valor + dessert.valor).toFixed(2);
+  }
+  const reviewOrder = () => {
     if (checkAllData()) {
-      valor = snack.valor + drink.valor + dessert.valor;
-      valor.toFixed(2).replace(".", ",");
-      let msg = `
-      Olá, gostaria de fazer o pedido:
-      - Prato: ${snack.escolha}
-      - Bebida: ${drink.escolha}
-      - Sobremesa: ${dessert.escolha}
-      Total: R$ ${snack.valor + drink.valor + dessert.valor}`;
-
-      const aboutSnacks = document.getElementById("aboutSnacks");
-      aboutSnacks.childNodes[1].textContent = snack.escolha;
-      aboutSnacks.childNodes[3].textContent = `R$ ${snack.valor}`;
-
-      const aboutDrink = document.getElementById("aboutDrink");
-      aboutDrink.childNodes[1].textContent = drink.escolha;
-      aboutDrink.childNodes[3].textContent = `R$ ${drink.valor}`;
-
-      const aboutDessert = document.getElementById("aboutDessert");
-      aboutDessert.childNodes[1].textContent = dessert.escolha;
-      aboutDessert.childNodes[3].textContent = `R$ ${dessert.valor}`;
-
-      const totalPrice = document.getElementById("totalPrice");
-      totalPrice.childNodes[3].textContent = `R$ ${valor.toFixed(2)}`;
-
-      const reviewOrder = document.getElementById("reviewCard");
-      reviewOrder.style.display = "flex";
+      const total = getOrderPrice();
+      cardConstructor(total);
     }
   };
-
+  const submitOrder = () => {
+    const total = getOrderPrice();
+    const userNameAndAdress = getNameAndAdress();
+    let msg = `Olá, gostaria de fazer o pedido:\n- Prato: ${snack.escolha}\n- Bebida: ${drink.escolha}\n- Sobremesa: ${dessert.escolha}\nTotal: R$ ${total}\n\nNome: ${userNameAndAdress.userName}\nEndereço: ${userNameAndAdress.userAdress}`;
+    window.location.href = `https://wa.me/5528999848929?text=${encodeURIComponent(msg)}`;
+  };
   const cancelOrder = () => {
     const reviewOrder = document.getElementById("reviewCard");
     reviewOrder.style.display = "none";
   };
+
   return {
-    submiteOrder,
     getElementData,
     run,
+    reviewOrder,
+    submitOrder,
     cancelOrder,
   };
 })();
-
 function snackCard(elemento) {
   snack = core.getElementData(elemento);
   core.run(".snacks", "indicadorSnackCards", elemento);
@@ -108,6 +106,6 @@ function dessertCard(elemento) {
   dessert = core.getElementData(elemento);
   core.run(".desserts", "indicadorDessertCards", elemento);
 }
-
-submitButton.addEventListener("click", core.submiteOrder);
+reviewButton.addEventListener("click", core.reviewOrder);
+submitButton.addEventListener("click", core.submitOrder);
 cancelButton.addEventListener("click", core.cancelOrder);
